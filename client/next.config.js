4985fb0cb803b3264095cee7535482887a66edc2
Albert ChenX <chenxeed@gmail.com>
2019-08-13 00:10:54 +0800

    build: retrieve env variables to client side

--- client/next.config.js
+++ client/next.config.js
@@ -1,11 +1,18 @@
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const withSass = require('@zeit/next-sass')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = withSass({
   webpack(config, options) {
     // Do not run type checking twice:
-    if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
-    //      
+    if (options.isServer) {
+      config.plugins.push(new ForkTsCheckerWebpackPlugin())
+    }
+    // Add dotenv variable
+    config.plugins.push(new Dotenv({
+      path: '../.env'
+    }))
+
     return config
   }
 })
