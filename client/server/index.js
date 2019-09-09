016b836fd4522b7280bd29f4afc189dd3463d3f1
Albert ChenX <chenxeed@gmail.com>
2019-09-09 13:13:40 +0800

    feat(client): add server script

---
+++ client/server/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const next = require('next')
+const bodyParser = require('body-parser')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+// require env variable
+require('dotenv').config({
+  path: '../.env'
+})
+
+app
+  .prepare()
+  .then(() => {
+    const server = express()
+    server.use(bodyParser.json())
+
+    server.get('/', async (req, res) => {
+      const actualPage = '/index'
+      app.render(req, res, actualPage)
+    })
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(process.env.CLIENT_PORT, err => {
+      if (err) throw err
+      console.log(`> Ready on ${process.env.CLIENT_HOST} port ${process.env.CLIENT_PORT}`)
+    })
+  })
+  .catch(ex => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
