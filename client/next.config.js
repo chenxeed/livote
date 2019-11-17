4676b579294d3403c9ff7c1eb366a0478680d4fd
Albert ChenX <chenxeed@gmail.com>
2019-11-18 01:01:27 +0800

    build(client): create alias path

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,7 @@
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const withSass = require('@zeit/next-sass')
 const Dotenv = require('dotenv-webpack')
+const path = require('path')
 
 module.exports = withSass({
   webpack(config, options) {
@@ -12,6 +13,12 @@ module.exports = withSass({
     config.plugins.push(new Dotenv({
       path: '../.env'
     }))
+
+    // Add alias paths
+    // Synchronize it with the alias defined in tsconfig.json
+    config.resolve.alias['@components'] = path.join(__dirname, 'components')
+    config.resolve.alias['@layout'] = path.join(__dirname, 'layout')
+    config.resolve.alias['@services'] = path.join(__dirname, 'services')
 
     return config
   }
