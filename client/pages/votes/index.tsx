c8daa11f6ed472b881b5a47ce16396cbcfb1c74e
Albert ChenX <chenxeed@gmail.com>
2019-11-21 01:51:31 +0800

    feat(client): show vote list

--- client/pages/votes/index.tsx
+++ client/pages/votes/index.tsx
@@ -1,14 +1,17 @@
 import { NextPage } from 'next'
 import Link from 'next/link'
-import { LayoutGeneral } from '../../layout/general'
-import { LayoutHeader } from '../../layout/header'
-import { PageTitle } from '../../components/page-title'
-import { AuthProvider, verify } from '../../services/auth'
-import { getAllVotes } from '../../services/votes'
+import { useState } from 'react'
+import { LayoutGeneral } from '@layout/general'
+import { LayoutHeader } from '@layout/header'
+import { PageTitle } from '@components/page-title'
+import { Button } from '@components/form'
+import { AuthProvider, verify } from '@services/auth'
+import { getAllVotes } from '@services/votes'
 import { PageProps } from '../types'
+import { Vote } from '@rootTypes/vote'
 
 interface VotePageProps extends PageProps {
-  votes: any
+  votes: Vote[]
 }
 
 const PageWrapper: NextPage<VotePageProps> = props => {
@@ -21,11 +24,18 @@ const PageWrapper: NextPage<VotePageProps> = props => {
 }
 
 const PageContent: NextPage<VotePageProps> = ({ votes }) => {
-  return <div className="w-full h-full mx-auto">
+
+  const [viewList, setViewList] = useState<Vote['_id'] | undefined>(undefined)
+
+  function showVoteList (voteId: Vote['_id']) {
+    setViewList(viewList === voteId ? undefined : voteId)
+  }
+
+  return <div className="w-full h-full">
     <PageTitle>Votes</PageTitle>
-    <div className="mx-auto">
+    <div className="container mx-auto">
       { votes && votes.length ?
-        <table className="table-auto">
+        <table className="table-auto w-full">
           <thead>
             <tr>
               <th className="px-4 py-2">Title</th>
@@ -34,13 +44,44 @@ const PageContent: NextPage<VotePageProps> = ({ votes }) => {
             </tr>
           </thead>
           <tbody>
-            { votes.map((vote: any) => {
-              return <tr key={ vote._id }>
-                <td className="border px-4 py-2">{ vote.title }</td>
-                <td className="border px-4 py-2">{ vote.description }</td>
-                <td className="border px-4 py-2">See list</td>
-              </tr>
+            { votes.map((vote) => {
+              const voteColumn = [
+                <tr key={ vote._id }>
+                  <td className="border px-4 py-2">{ vote.title }</td>
+                  <td className="border px-4 py-2">{ vote.description }</td>
+                  <td className="border px-4 py-2">
+                    <Button onClick={ () => showVoteList(vote._id) }>
+                      { viewList === vote._id ? `Hide List` : `Show List` }
+                    </Button>
+                  </td>
+                </tr>
+              ]
+              if (viewList === vote._id) {
+                if (vote.lists.length) {
+                  vote.lists.forEach(list => {
+                    voteColumn.push(
+                      <tr className="bg-gray-100" key={ list._id }>
+                        <td className="border px-4 py-2">{ list.title }</td>
+                        <td className="border px-4 py-2">{ list.description }</td>
+                        <td></td>
+                      </tr>
+                    )
+                  })  
+                } else {
+                  voteColumn.push(
+                    <tr className="bg-gray-100" key="empty-vote-list">
+                      <td className="text-center" colSpan={3}>Vote List empty</td>
+                    </tr>
+                  )
+                }
+              }
+              return voteColumn
             })}
+            <tr>
+              <td colSpan={ 3 }>
+                <Link href="/votes/add"><Button>+</Button></Link>
+              </td>
+            </tr>
           </tbody>
         </table>
       :
